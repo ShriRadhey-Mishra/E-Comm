5968905fd80fc67ae83736408e6b69e315372acf
ShriRadhey-Mishra <radheymishra13@gmail.com>
2026-03-26 13:23:53 +0530

    changes irrelevant to core app
    as of now

--- src/test/kotlin/com/example/e_comm/ECommApplicationTests.kt
+++ src/test/kotlin/com/example/e_comm/ECommApplicationTests.kt
@@ -1,13 +1,18 @@
 package com.example.e_comm
 
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class ECommApplicationTests {
-
-	@Test
-	fun contextLoads() {
-	}
+    //
+//	@Test
+//	fun contextLoads() {
+//	}
+    @Test
+    fun testAddition() {
+        assertEquals(4, 2 + 2)
+    }
 
 }
